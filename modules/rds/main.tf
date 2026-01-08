5969e7666b89ceda37fa4e10a4582c9c205e3b0a
ShenLoong99 <yithuettsk29@gmail.com>
2026-01-08 23:10:02 +0800

    comment for reminding about multi az and its costs

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -18,5 +18,6 @@ resource "aws_db_instance" "mysql" {
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [var.rds_sg_id]
   skip_final_snapshot    = true
-  multi_az               = false # Keeping it simple/cheap for the project
+  # multi_az               = true # For production, consider enabling Multi-AZ
+  multi_az               = false # Disabled for cost-saving in dev/test
 }
